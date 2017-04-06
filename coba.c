5bf63d82b3f89b8fa06d95b832a23ab2457d1338
rizkay123 <pilci_calpus@yahoo.com>
2017-04-06 13:44:12 +0700

    Add files via upload

---
+++ coba.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+#include "coba.h"
+struct bilReal{
+   int x;
+   int y;
+}
+
+BilReal newBilReal(int angka, int koma){
+
+}
+int main ()
+{
+    char input[100];
+    printf("Masukan Password: "); gets(input);
+    printf("safdsa");
+    inject_and_bypass();
+    if (strcmp(input,"abcde") == 0){
+         printf ("PASSWORD BERHASIL!");
+    }else{
+         printf ("PASSWORD SALAH!\n");
+    }
+    getch();
+    return 0;
+}
+
+
+
